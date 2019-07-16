66b70722face4494f7b0c94473532e1bce733051
huangguobin <huangguobin@172.18.205.58>
2019-07-16 10:15:41 +0800

    修改image的字段

--- src/com/cavlib/dao/ImageMapper.java
+++ src/com/cavlib/dao/ImageMapper.java
@@ -3,6 +3,6 @@ package com.cavlib.dao;
 import java.util.List;
 
 public interface ImageMapper {
-    public List<String> getImageByPostId(int id);
-    public Boolean addImage(int id, String url);
+    public List<String> getImageByPostId(int post_id);
+    public Boolean addImage(int post_id, String url);
 }
