387d344744bf205f395a7f86a0ea8c98d1d78ff2
huangguobin <huangguobin@172.18.205.58>
2019-07-16 14:59:31 +0800

    更新post接口

--- src/com/cavlib/dao/PostMapper.java
+++ src/com/cavlib/dao/PostMapper.java
@@ -11,8 +11,9 @@ public interface PostMapper {
 	public Post getPostById(int post_id);
 	public Post getPostByTitle(String title);
 	public List<Post> getPostByTime(Date time);
-	public List<Post> getPostByUserId(int user_d);
+	public List<Post> getPostByUserId(int user_id);
 	public List<Post> getPostByType(String type);
+	public List<Post> getFivePost(int begin, String content, String type);
 	
 	public boolean publishPost(@Param("post") Post post);
 	
