528e3b91c1200267b2a421c870406a106936057a
BoogieMan <43433640+NO1excavator@users.noreply.github.com>
2019-07-16 11:50:35 +0800

    Create create_table.sql
    创建数据库表格的语句

---
+++ create_table.sql
@@ -0,0 +1,33 @@
+CREATE TABLE user(
+   user_id INT NOT NULL AUTO_INCREMENT,
+   name VARCHAR(30) NOT NULL,
+   password VARCHAR(30) NOT NULL,
+   is_manager TINYINT NOT NULL,
+   is_baned TINYINT NOT NULL,
+   PRIMARY KEY (user_id)
+);
+
+CREATE TABLE post(
+   post_id INT NOT NULL AUTO_INCREMENT,
+   title VARCHAR(100) NOT NULL,
+   time  TIMESTAMP NOT NULL DEFAULT current_timestamp
+   content TEXT NOT NULL,
+   link  VARCHAR(100) NOT NULL,
+   user_id INT NOT NULL,
+   type VARCHAR(10) NOT NULL,
+   PRIMARY KEY (post_id)
+);
+
+CREATE TABLE comment(
+   user_id INT NOT NULL,
+   post_id INT NOT NULL,
+   time TIMESTAMP NOT NULL DEFAULT current_timestamp
+   content TEXT NOT NULL,
+   PRIMARY KEY (user_id, post_id, time)
+);
+
+CREATE TABLE image(
+   post_id INT NOT NULL,
+   url VARCHAR(100) NOT NULL,
+   PRIMARY KEY (post_id, url)
+);
