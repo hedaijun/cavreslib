4aae5e542ffd9c383d0a91a5ad07fead96d65496
p <p@windows10.microdone.cn>
2019-07-17 16:32:41 +0800

    update

--- src/com/cavlib/service/impl/AdminManagePostServiceIpml.java
+++ src/com/cavlib/service/impl/AdminManagePostServiceIpml.java
@@ -1,8 +1,11 @@
 package com.cavlib.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.cavlib.dao.PostMapper;
 import com.cavlib.service.AdminManagePostService;
 
+@Service
 public class AdminManagePostServiceIpml implements AdminManagePostService{
 
 	private PostMapper postMapper;
