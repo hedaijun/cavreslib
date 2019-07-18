8fd393a524e9d0f36aabe3104012a561f1224d0d
huangguobin <huangguobin@172.18.205.58>
2019-07-18 15:33:37 +0800

    增加user通过id找name的接口

--- src/com/cavlib/dao/UserMapper.java
+++ src/com/cavlib/dao/UserMapper.java
@@ -9,6 +9,7 @@ public interface UserMapper {
 	public Integer checkUserByUserName(String name);
 	public String getPasswordByUserName(String name);
 	public User getUserInfoByUserName(String name);
+	public String getUserNameByUserId(Integer user_id);
 	public Boolean setIsBaned(@Param("is_baned") Boolean is_baned, @Param("name") String name);
 	public Boolean addUser(@Param("name") String name, @Param("password")String password);
 	
