44972c957e4c43d6e0d4263163ca086fc884673b
1481666070 <52899042+1481666070@users.noreply.github.com>
2019-07-22 10:35:25 +0800

    Add files via upload

---
+++ src/com/cavlib/handler/ManageUserController.java
@@ -0,0 +1,39 @@
+package com.cavlib.handler;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cavlib.beans.User;
+import com.cavlib.service.ManageUserService;
+
+@Controller
+public class ManageUserController {
+	@Autowired
+	private ManageUserService manageUserService;
+	
+	@RequestMapping("/setbane")
+	public String setBane(Boolean is_baned,String name) {
+		//System.out.println("ok");
+		manageUserService.setbane(is_baned, name);
+		return "redirect:getpage";
+	}
+	@RequestMapping("/getpage")
+	public String getUsers(Map<String,Object> map,HttpServletRequest request,HttpServletResponse response,HttpSession session) {
+		int page=1;
+		if(request.getParameter("pageindex")!=null) page= Integer.valueOf(request.getParameter("pageindex"));
+		if(page<1) page=1;
+		session.setAttribute("pageindex", page);
+		List<User> users = manageUserService.loadPage(page);
+		map.put("users", users);
+		return "userManager";
+	}
+	
+}
