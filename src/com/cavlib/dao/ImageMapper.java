eb13653e6b215fed76a6719ceb6d3187d7b00e27
huangguobin <huangguobin@172.18.218.6>
2019-07-23 10:00:57 +0800

    增加image mapper删除接口

--- src/com/cavlib/dao/ImageMapper.java
+++ src/com/cavlib/dao/ImageMapper.java
@@ -6,6 +6,6 @@ import com.cavlib.beans.Image;
 
 public interface ImageMapper {
     public List<String> getImageByPostId(int post_id);
-    public Boolean addImage(int post_id, String url);
     public void addImage(Image image);
+    public Boolean deleteImage(int post_id);
 }
